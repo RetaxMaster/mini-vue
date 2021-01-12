559612ae94b2173197d79410d31dc372335e34f6
RetaxMaster <retaxmaster@hotmail.com>
2021-01-12 17:27:43 -0600

    Adicion del objeto Reflect

--- platzi.js
+++ platzi.js
@@ -9,11 +9,17 @@ class PlatziReactive {
 
             get(target, name) {
 
-                if (name in target)
-                    return target[name];
+                if (Reflect.has(target, name))
+                    return Reflect.get(target, name);
 
                 console.warn(`La porpiedad [${name}] no existe`);
                 return "";
+
+            },
+
+            set(target, name, value) {
+
+                Reflect.set(target, name, value)
 
             }
 
@@ -29,15 +35,30 @@ class PlatziReactive {
 
         });
 
+        document.querySelectorAll("*[p-model]").forEach(el => {
+
+            const name = el.getAttribute("p-model");
+            this.pModel(el, this.$data, name);
+
+            el.addEventListener("input", () => {
+                Reflect.set(this.$data, name, this.value);
+            });
+
+        });
+
     }
 
     pText(el, target, name) {
 
-        el.innerText = target[name]
+        el.innerText = Reflect.get(target, name);
 
     }
 
-    pModel() {}
+    pModel(el, target, name) {
+
+        el.value = Reflect.get(target, name);
+
+    }
 
 }
 
