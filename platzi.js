c18944ae9a6699fbd8e704f1a70a5c636b57eb07
RetaxMaster <retaxmaster@hotmail.com>
2021-01-12 16:59:57 -0600

    Adicion de un proxy para validacion durante el acceso a las propiedades de p-text

--- platzi.js
+++ platzi.js
@@ -4,13 +4,28 @@ class PlatziReactive {
 
         this.origen = options.data();
 
+        // Destino
+        this.$data = new Proxy(this.origen, {
+
+            get(target, name) {
+
+                if (name in target)
+                    return target[name];
+
+                console.warn(`La porpiedad [${name}] no existe`);
+                return "";
+
+            }
+
+        });
+
     }
 
     mount() {
 
         document.querySelectorAll("*[p-text]").forEach(el => {
 
-            this.pText(el, this.origen, el.getAttribute("p-text"));
+            this.pText(el, this.$data, el.getAttribute("p-text"));
 
         });
 
